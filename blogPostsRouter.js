ad5a152ec2f84b123a48bfc6164370afb2f337f5
jwoleslagle <30732086+jwoleslagle@users.noreply.github.com>
2018-03-05 15:58:06 -0500

    Converted data storage to mongo

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -4,21 +4,36 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const {BlogPosts} = require('./models');
-
-// we're going to add some BlogPosts to BlogPosts
-// so there's some data to look at
-BlogPosts.create(
-    'Feeling happy', 'Today I am feeling happy - oh so happy!', 'Rando Blogsman');
-BlogPosts.create(
-    'Feeling sad', 'Today I am feeling sad - so so very sad.', 'Rando Blogsman');
+const {BlogPost} = require('./models');
 
 // send back JSON representation of all BlogPosts
 // on GET requests to root
 router.get('/', (req, res) => {
-  res.json(BlogPosts.get());
+  BlogPost
+    .find()
+    .then(posts => {
+      res.json({
+        posts: posts.map(
+          post => post.serialize())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
 
+router.get('/:id', (req,res) => {
+  BlogPost
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(blogPost => res.json(blogPost.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
 
 // when new blog post added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
@@ -34,15 +49,25 @@ router.post('/', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
-  res.status(201).json(item);
+  BlogPost
+  .create({
+    title: req.body.title, 
+    content: req.body.content, 
+    author: req.body.author
+  })
+  .then(blogPost => res.status(201).json(blogPost.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  })
 });
 
 // Delete BlogPosts (by id)!
 router.delete('/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog post item \`${req.params.ID}\``);
-  res.status(204).end();
+  BlogPost
+  .findByIdAndRemove(req.params.id)
+  .then(restaurant => res.status(204).end())
+  .catch(err => res.status(500).json({ message: 'Internal server error'}))
 });
 
 // when PUT request comes in with updated blog post, ensure has
@@ -51,30 +76,31 @@ router.delete('/:id', (req, res) => {
 // of that, log error and send back status code 400. otherwise
 // call `BlogPosts.updateItem` with updated blog post.
 router.put('/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = (
-      `Request path id (${req.params.id}) and request body id `
+      `Request path id (${req.params.id}) and request body id ` +
       `(${req.body.id}) must match`);
     console.error(message);
-    return res.status(400).send(message);
+    return res.status(400).json({ message: message });
   }
   console.log(`Updating blog post item \`${req.params.id}\``);
-  const updatedItem = BlogPosts.update({
-    id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
+   // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
   });
-  res.status(204).end();
-})
+
+  BlogPost
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+'use strict';
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// Mongoose internally uses a promise-like object,
+// but its better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
+
+// config.js is where we control constants for entire
+// app like PORT and DATABASE_URL
+const { PORT, DATABASE_URL } = require('./config');
+const { BlogPost } = require('./models');
 
 const app = express();
+app.use(bodyParser.json());
 
 const blogPostsRouter = require('./blogPostsRouter');
 
@@ -19,9 +33,54 @@ app.get('/', (req, res) => {
 // we'll route them to the express
 // router instances we've imported. Remember,
 // these router instances act as modular, mini-express apps.
-app.use('/blog-posts', blogPostsRouter);
+app.use('/posts', blogPostsRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
 
